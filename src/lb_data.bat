0fe7250c6c4bf3120508ed62d463abf20d78b3ff
Ondřej Vejtasa <ondrej.vejtasa@urc-systems.cz>
2018-10-18 23:41:40 +0200

    REMOVE - old version Liquibase

--- src/lb_data.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-REM SIMULEB
-echo Installing SIMULEB Database... & @call ../liquibase/liquibase.bat --changeLogFile=install/insert.xml --defaultsFile=liquibase.properties validate clearCheckSums update && echo SIMULEB Database was successfully installed. & echo.
-if not %errorlevel%==0 pause
--- src/lb_install.bat
+++ src/lb_install.bat
@@ -1,4 +1,10 @@
 @echo off
+for /f "tokens=3-5 delims=:\ " %%i in ('FINDSTR /B /C:"url" .\liquibase.properties') do (
+  set url=%%i:%%j:%%k
+)
+SET PGPASSWORD=foa
+echo %url%
+call "C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -d %url% -U foa -w -c "TRUNCATE TABLE public.databasechangelog; DROP schema IF EXISTS main CASCADE; DROP schema IF EXISTS reality CASCADE; DROP schema IF EXISTS economy CASCADE; DROP schema IF EXISTS technology CASCADE; "
 
 REM FOA
 echo Installing FOA Database... & @call ../liquibase/liquibase.bat --changeLogFile=install/install.xml --defaultsFile=liquibase.properties validate clearCheckSums update && echo FOA Database was successfully installed. & echo.
